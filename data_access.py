6517bda390f8ffc287f836622800e7fdf0d28abf
Emily Ryalls <emilyryalls99@gmail.com>
2025-03-29 19:22:53 +0000

    CREATE: data_access.py

---
+++ data_access.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import sys
+
+if sys.platform == "win32":
+    mysql_password = "password"
+else:
+    mysql_password = ""
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=mysql_password,
+  database="get_into_tech_c1_2025"
+)
