55b2ea0d13193dce36a44493b712222d485818e6
Emily Ryalls <emilyryalls99@gmail.com>
2025-03-29 11:30:39 +0000

    DELETE: template files

--- application/fake_data.py
+++
@@ -1,7 +0,0 @@
-products = [
-    {'ID': 0, 'name': "Star Widget", 'price': 5.99, 'image_src': 'image_0.jpg'},
-{'ID': 1, 'name': "Flying Widget", 'price': 7.99, 'image_src': 'image_1.jpg'},
-{'ID': 2, 'name': "Spinning Widget", 'price': 5.45, 'image_src': 'image_2.jpg'}
-]
-
-people = []
--- application/forms/register_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class RegisterForm(FlaskForm):
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    submit = SubmitField('Add Name')
-
